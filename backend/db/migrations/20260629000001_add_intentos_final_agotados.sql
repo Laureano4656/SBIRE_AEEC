a3ac5fff33cba0e5f431162fc94806b0c6890a1d
Agustinp <proiaagustin@gmail.com>
2026-06-29 16:56:16 +0000

    Fix the migration add_intentos_final_agotados

--- backend/db/migrations/20260629000001_add_intentos_final_agotados.sql
+++ backend/db/migrations/20260629000001_add_intentos_final_agotados.sql
@@ -4,13 +4,13 @@ ALTER TYPE public.origen_alerta_enum ADD VALUE IF NOT EXISTS 'intentos_final_ago
 
 INSERT INTO intentos_finales (cursada_id, numero_intento, nota, fecha, aprobado)
 VALUES (3,1,2,CURRENT_TIMESTAMP, false),
-(3,2,3,CURRENT_TIMESTAMP, false)
+(3,2,3,CURRENT_TIMESTAMP, false);
 
 
 INSERT INTO asignacion_encuesta (estudiante_id, periodo_lectivo, completado, evento_id, borrador)
 VALUES
 -- ======== ESTUDIANTE 1 ========
 -- Cuatrimestre 1 (20261)
-(1, 20271, false, 6, false),
+(1, 20271, false, 6, false)
 
 -- migrate:down
